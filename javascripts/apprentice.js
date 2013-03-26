71314bd5eee849c02980239e7f2fe04b149036ad
Joel Smith <jsumnersmith@gmail.com>
2013-03-26 16:31:06 -0400

    Updated to Foundation 4 and revised some content based on what we perceived in the interviews and emails

--- javascripts/apprentice.js
+++ javascripts/apprentice.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
     $('.footer-accordion-content').slideToggle('blind');
     $(this).toggleClass('open');
   });
+  var breakpoint = $("<div class='top-bar-js-breakpoint'/>").appendTo("body");
+  function apprenticeshipHyphen() {
+    if ($(window).width() < breakpoint.width()) {
+      $('.apprentitle').text('Appren- ticeship');
+    } else {
+      $('.apprentitle').text('Apprenticeship');
+    }
+  }
+  apprenticeshipHyphen();
+  $(window).on('resize', apprenticeshipHyphen);
 });
